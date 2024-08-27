b02497259dceee35112d0268cb8ae2ce4ae3c51f
hernanveyret <hernanveyret@hotmail.com>
2024-08-27 19:32:09 -0300

    Mejoras

--- src/Componentes/Settings.js
+++ src/Componentes/Settings.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './settings.css';
-const Settings = ({setSettings}) => {
+const Settings = ({settingOptions}) => {
   return (
-    <div className="containerSettings">
-      <button className="btn-cerrar-settings" onClick={() => {setSettings(false)}}>X</button>
-      <h2>Configuraciones</h2>
+    <div className="containerSettings" id="panelSetting">
+      <button className="btn-cerrar-settings" onClick={settingOptions}>X</button>
+      <div className="nav"><h3>Configuraciones</h3></div>
+      <nav>
+        <form>
+          <label><div>Nodo nocturno <input type="radio" name="settingRadio" id="nocturno" /></div></label>
+          <label><div>Por mes<input type="radio" name="settingRadio" id="mes" /></div></label>
+          <label><div>Por dia<input type="radio" name="settingRadio" id="dia"/></div></label>
+        </form>
+      </nav>
     </div>
   )
 }
